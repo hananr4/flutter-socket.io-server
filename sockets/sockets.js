9edcd073bb9f78edeaf1bedcf8a66705b476437b
Hanan Rodriguez <hanan.rodriguez@greensoft.com.ec>
2021-04-17 19:52:39 -0500

    Cambios sobre bandas

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,8 +1,24 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+//console.log('init server');
+
+bands.addBand(new Band('Don Medardo'));
+bands.addBand(new Band('The Beatles'));
+bands.addBand(new Band('Menudo'));
+bands.addBand(new Band('Doors'));
+bands.addBand(new Band('Queen'));
+
+//console.log(bands);
+
 // Mensajes de sockets
 io.on('connection', client => {
     console.log(`Cliente conectado ${client.id}`);
-    console.log(client.id);
+
+    client.emit('active-bands', bands.getBands());
+
 
     client.on('disconnect', () => { 
         console.log(`Cliente desconectado ${client.id}`);
@@ -10,8 +26,31 @@ io.on('connection', client => {
 
 
     client.on('mensaje', function (payLoad) {
-        console.log(payLoad);
         io.emit('mensaje', {admin: 'Nuevo mensaje'});
     });
+
+
+    client.on('vote-band', function (payLoad) {
+        bands.voteBand(payLoad.id);
+        io.emit('active-bands', bands.getBands()); 
+    });
+    
+    client.on('add-band', function (payLoad) {
+        bands.addBand(new Band(payLoad.name));
+        io.emit('active-bands', bands.getBands()); 
+    });
+    
+    client.on('delete-band', function (payLoad) {
+        bands.deleteBand(payLoad.id);
+        io.emit('active-bands', bands.getBands()); 
+    });
+    
+
+
+    // client.on('emitir-mensaje', (payload) => {
+    //     //io.emit('nuevo-mensaje', payload); // emite a todos!!
+    //     console.log(payload);
+    //     client.broadcast.emit('nuevo-mensaje', payload); // emite a todos menos el que lo emitió
+    // });
 
   });
